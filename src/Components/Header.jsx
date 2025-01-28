a3ec3de44889d2dc56a99663ccb541b67dcb9e59
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-28 12:09:48 +0530

    Create Header UI

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full shadow-md">
+      <header className="w-[1200px] mx-auto   h-16 place-content-between  flex  items-center ">
+        <h1 className="text-2xl font-bold">Shopping Cart</h1>
+        <div className="flex relative cursor-pointer   ">
+          <MdOutlineShoppingCart className="text-2xl " />
+          <h2 className=" absolute -top-2 -right-2 bg-black text-white rounded-4xl w-5 h-5  flex place-content-center items-center ">
+            2
+          </h2>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;

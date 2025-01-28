014834edbcabd8db993044b541f01f7f7c96f312
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-28 16:24:04 +0530

    Link Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Products from "./Components/Products";
 import Cart from "./Components/Cart";
+
 function App() {
   return (
     <>
-      <Header />
-      {/* <Products /> */}
-      <Cart />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
